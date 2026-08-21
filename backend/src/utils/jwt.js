d0fbc4619b3c23a22fc22c333660a6c8afa1646e
Faiyaz Noor <noorfaiyaz317@gmail.com>
2026-08-21 16:57:49 +0600

    Express Backend Setup

---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+function signToken(userId) {
+return jwt.sign(
+{ userId },
+process.env.JWT_SECRET,
+{ expiresIn: '7d' }
+);
+}
+module.exports = { signToken };
