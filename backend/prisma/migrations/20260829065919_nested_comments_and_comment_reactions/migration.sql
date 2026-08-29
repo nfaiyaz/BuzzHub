2332ca17a62bacf620e67a96bf3fab29f3ff7f1f
Faiyaz Noor <noorfaiyaz317@gmail.com>
2026-08-29 13:02:00 +0600

    Updated schema for nested comments and comment reactions

---
+++ backend/prisma/migrations/20260829065919_nested_comments_and_comment_reactions/migration.sql
@@ -0,0 +1,27 @@
+-- AlterTable
+ALTER TABLE "Comment" ADD COLUMN     "parentId" TEXT;
+
+-- CreateTable
+CREATE TABLE "CommentReaction" (
+    "userId" TEXT NOT NULL,
+    "commentId" TEXT NOT NULL,
+    "emoji" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CommentReaction_pkey" PRIMARY KEY ("userId","commentId")
+);
+
+-- CreateIndex
+CREATE INDEX "CommentReaction_commentId_idx" ON "CommentReaction"("commentId");
+
+-- CreateIndex
+CREATE INDEX "Comment_parentId_idx" ON "Comment"("parentId");
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CommentReaction" ADD CONSTRAINT "CommentReaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CommentReaction" ADD CONSTRAINT "CommentReaction_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
